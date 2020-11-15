1ebf1478d88c2f1faf171e3b3d1a90fea87472f0
mrabusalah <abdullah.adel@progressoft.com>
2020-11-15 19:48:02 +0200

    feat: adding new soluations

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int tc = sc.nextInt();
-        int cnt = 0;
-        while (tc-- > 0) {
-            int p = sc.nextInt();
-            int q = sc.nextInt();
-            if (q - p >= 2)
-                cnt++;
-        }
-        System.out.println(cnt);
+
     }
 }
